c1180f81dd225f8694160bb6f911f6e7cda22ba9
NOIRET Maxime <maxime.noiret@etu.univ-amu.fr>
2025-01-08 18:02:34 +0100

    added js to switch Theme

---
+++ page3/switchTheme.js
@@ -0,0 +1,11 @@
+let colors_css_files = ["/css/colors_dark.css", "/css/colors_light.css"]
+
+function switchCSS() {
+    const cssLink = document.getElementById("cssColors");
+
+    if (cssLink) {
+        cssLink.href == colors_css_files[0] ? cssLink.href = colors_css_files[1] : colors_css_files[0];
+    } else {
+        console.warn('CSS file not found:', existingCssHref);
+    }
+}
