c42a15ae2cd4cf551ee921f4935ceef1004280f1
NOIRET Maxime <maxime.noiret@etu.univ-amu.fr>
2025-01-08 18:09:12 +0100

    made theme swither better (work with more themes)

--- page3/switchTheme.js
+++ page3/switchTheme.js
@@ -1,10 +1,13 @@
 let colors_css_files = ["/css/colors_dark.css", "/css/colors_light.css"]
+let currIndex = 0;
 
 function switchCSS() {
     const cssLink = document.getElementById("cssColors");
 
     if (cssLink) {
-        cssLink.href == colors_css_files[0] ? cssLink.href = colors_css_files[1] : colors_css_files[0];
+        currIndex == colors_css_files.length-1 ? currIndex = 0 : ++currIndex;
+        cssLink.href = colors_css_files[currIndex];
+
     } else {
         console.warn('CSS file not found');
     }
